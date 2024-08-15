d0f98ea046151f7ce7d5048debe7c1d124a8b03d
thiagonmiziara <thiagonmiziara@gmail.com>
2024-08-15 18:01:16 -0300

    feat: create conection database

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ from models.meal import Meal
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "mysql+pymysql://admin:admin123@127.0.0.1:3306/meal-record"
+)
 db.init_app(app)
 
 
