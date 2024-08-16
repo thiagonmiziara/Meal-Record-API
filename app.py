7dbad5b4d008b36d8ec3953aa82009ee9dab07f2
thiagonmiziara <thiagonmiziara@gmail.com>
2024-08-16 09:27:57 -0300

    feat: add validation in description

--- app.py
+++ app.py
@@ -28,6 +28,10 @@ def meal_by_id(meal_id):
 @app.route("/meal", methods=["POST"])
 def create_meal():
     data = request.get_json()
+
+    if len(data["description"]) > 120:
+        return {"error": "Description must be less than 120 characters"}, 400
+
     new_meal = Meal(
         name=data["name"],
         description=data["description"],
@@ -42,12 +46,21 @@ def create_meal():
 @app.route("/meal/<int:meal_id>", methods=["PUT"])
 def update_meal(meal_id):
     meal = Meal.query.get(meal_id)
+
     if not meal:
         return {"error": "Meal not found"}, 404
+
     data = request.get_json()
+
+    if not data:
+        return {"error": "No data provided"}, 400
+
+    if "description" in data and len(data["description"]) > 120:
+        return {"error": "Description must be less than 120 characters"}, 400
+
     meal.name = data.get("name", meal.name)
     meal.description = data.get("description", meal.description)
-    meal.datetime = data.get("datetime", datetime.datetime.now())
+    meal.datetime = data.get("datetime", meal.datetime)
     meal.dieting = data.get("dieting", meal.dieting)
     db.session.commit()
     return {"message": "Meal updated successfully"}
